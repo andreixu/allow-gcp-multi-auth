3b4a89e623f5d2a7da12d919ee96c97309874a02
andrei.iscu <andrei.iscu@flowos.com>
2022-09-14 17:27:44 +0300

    first test release

--- src/gcp.ts
+++ src/gcp.ts
@@ -3,6 +3,7 @@ import * as fs from "fs";
 
 export async function clearGcpAuth() {
   const filePath = process.env["GITHUB_ENV"] || "";
+  core.info(filePath);
   if (filePath) {
     const fileData = fs.readFileSync(filePath, "utf-8");
     core.info(fileData);
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 //import * as core from "@actions/core";
 import * as gcp from "./gcp";
 
-export async function run(): Promise<void> {
+async function run(): Promise<void> {
   await gcp.clearGcpAuth();
 }
+
+run();
