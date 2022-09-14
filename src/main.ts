83f8665ab3dcddff081e673c24776c3d199f28f1
andrei.iscu <andrei.iscu@flowos.com>
2022-09-14 17:32:15 +0300

    first test release

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-//import * as core from "@actions/core";
+import * as core from "@actions/core";
 import * as gcp from "./gcp";
 
 async function run(): Promise<void> {
+  core.info("STarting custom action...");
   await gcp.clearGcpAuth();
+  core.warning("Should have output");
 }
 
 run();
